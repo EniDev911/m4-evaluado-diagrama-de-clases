d305add6857963f1b2bd1cee56b6d0f476bcdc40
enidev911 <enidev911@gmail.com>
2024-09-20 18:49:44 -0300

    up: encuesta

--- encuesta.py
+++ encuesta.py
@@ -1,12 +1,13 @@
 from listado_respuestas import ListadoRespuestas
 from pregunta import Pregunta
 
-class Encuesta():
+
+class Encuesta:
     def __init__(self, nombre):
         self.__nombre = nombre
         self.__preguntas = []
         self.__respuestas = []
-    
+
     def agregar_pregunta(self, pregunta: Pregunta):
         self.__preguntas.append(pregunta)
 
@@ -16,11 +17,11 @@ class Encuesta():
     @property
     def nombre(self):
         return self.__nombre
-    
+
     @nombre.setter
     def nombre(self, new_nombre):
         self.__nombre = new_nombre
-    
+
     def mostrar_encuesta(self):
         print(self.nombre.upper())
         print("------------------")
@@ -30,6 +31,7 @@ class Encuesta():
 
     def mostrar_respuestas(self):
         print(self.__respuestas)
+
 
 class EncuestaLimitadaEdad(Encuesta):
     EDAD_MINIMA = 18
@@ -42,9 +44,12 @@ class EncuestaLimitadaEdad(Encuesta):
     @staticmethod
     def validar_edad(edad):
         return (
-            True if edad > EncuestaLimitadaEdad.EDAD_MINIMA and edad < EncuestaLimitadaEdad.EDAD_MAXIMA
+            True
+            if edad > EncuestaLimitadaEdad.EDAD_MINIMA
+            and edad < EncuestaLimitadaEdad.EDAD_MAXIMA
             else False
         )
+
     @property
     def edad(self):
         if self.validar_edad(self.__edad):
@@ -56,6 +61,7 @@ class EncuestaLimitadaEdad(Encuesta):
         else:
             print("No cumples con el rango de edad para responder esta encuesta.")
 
+
 class EncuestaLimitadaRegion(Encuesta):
     def __init__(self, nombre):
         super().__init__(nombre)
@@ -63,24 +69,29 @@ class EncuestaLimitadaRegion(Encuesta):
         self.__regiones = [region for region in range(1, 17)]
 
     def agregar_respuestas(self, respuestas):
-        if self.validar_edad(self.__region):
+        if self.__region in self.__regiones:
             super().agregar_respuestas(respuestas)
         else:
-            print("No cumples con el rango de edad para responder esta encuesta.")
+            print(
+                "No cumples con ser de una región válida para responder esta encuesta."
+            )
+
+
 if __name__ == "__main__":
 
     e_normal = Encuesta("encuesta demo 1")
-    p1 = Pregunta('¿Cómo se dice hola en inglés?', requerida=False)
+    p1 = Pregunta("¿Cómo se dice hola en inglés?", requerida=False)
     p1.agregar_alternativa("Hi")
     p1.agregar_alternativa("Yes")
     p1.agregar_alternativa("Thanks", "(esto es un distractor)")
 
-    p2 = Pregunta('¿Cómo se dice serpiente en inglés?', requerida=False)
+    p2 = Pregunta("¿Cómo se dice serpiente en inglés?", requerida=False)
     p2.agregar_alternativa("Dog")
     p2.agregar_alternativa("Cat")
     p2.agregar_alternativa("Snake", "(esta puede tener sentido)")
 
     from usuario import Usuario
+
     e_normal.agregar_pregunta(p1)
     e_normal.agregar_pregunta(p2)
     user1 = Usuario("marco.contreras90@mail.com", 19, region=10)
